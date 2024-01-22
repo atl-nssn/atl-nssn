5ecbc37842dcd824cfc6a83ec46709d48b1bf484
Aditya <adityabagari77@gmail.com>
2024-01-22 17:40:22 +0530

    22-01-2024 05:40 PM

--- index.js
+++ index.js
@@ -19,3 +19,35 @@ function showSlides() {
   dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 5000);
 }
+
+// Navigation Button Functions
+
+// Home 
+const home_view = () => {
+  var home_view = document.getElementById("home_view");
+  home_view.scrollIntoView();
+}
+
+// About Us
+const abtUs = () =>{
+  var about_us_view = document.getElementById("about_div");
+  about_us_view.scrollIntoView();
+}
+
+// Recent Events
+const events_view = () =>{
+  var events_view = document.getElementById("recentevents_view")
+  events_view.scrollIntoView();
+}
+
+// Timeline
+const timeline_view = () => {
+  var timeline_view = document.getElementById("timeline_view")
+  timeline_view.scrollIntoView(); 
+}
+
+// Contact Us
+const contact_us_view = () => {
+  var contact_us_view = document.getElementById("contact_us_view")
+  contact_us_view.scrollIntoView();
+}
